d57529294e9f5e54214101e79e707527e3d9ff9b
MauroUCM <maumar03@ucm.es>
2024-10-18 18:36:58 +0200

    reemplazo shooter

--- Juego/src/objetos/Shooting/shooter.js
+++ Juego/src/objetos/Shooting/shooter.js
@@ -1,18 +1,23 @@
-//Hola Toni, soy JUlián,
-//Ciclyzar ////Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar
-//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//CiclyzarCiclyzar //Ciclyzar //Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar/
-//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar
-//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar//Ciclyzar
-
-
 import Bullet, {Bullet} from './bullet.js'
 
 export default class Shooter extends Phaser.GameObject.Sprite {
+    fire(shooter, target) {
+        let startX = shooter.x; //posicion de la bala al ser disparado
+        let  startY = shotter.y;
+        let direction = Math.atan((target.x - this.x) / (target.y - this.y));
 
+        this.setRotation(shooter.rotation);
 
-    generateBullet(shooter, target) {
-
-
+        if (target.y >= this.y) {
+            this.xSpeed = this.speed * Math.sin(this.direction);
+            this.ySpeed = this.speed * Math.cos(this.direction);
+        }
+        else {
+            this.xSpeed = -this.speed * Math.sin(this.direction);
+            this.ySpeed = -this.speed * Math.cos(this.direction);
+        }
+        this.setActive(true);
+        this.setVisible(true);
 
 
 
