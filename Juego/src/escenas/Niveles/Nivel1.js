6d68f7a6db200dde2e7a281f9799747321562b61
Haoshuang <haohou@ucm.es>
2024-10-21 01:15:58 +0200

    Tilemap(falta colision con la pared)

--- Juego/src/escenas/Niveles/Nivel1.js
+++ Juego/src/escenas/Niveles/Nivel1.js
@@ -12,26 +12,42 @@ export default class Animation extends Phaser.Scene {
 	}
 	
 	preload(){
-		this.load.image('suelo', 'assets/suelo.gif');
+		//this.load.image('suelo', 'assets/suelo.gif');
 		this.load.image('Player', 'assets/player.png');
+		this.load.tilemapTiledJSON('mapa1', 'assets/map/mapa1.json');
+		this.load.image('tileset', 'assets/map/mapTiles.png');
+
 	}
 	
-	/**
+	/**  
 	* Creación de los elementos de la escena principal de juego
 	*/
 	create() {
 		console.log("me he creado", this.scene.key);
 
 		//Imagen de fondo
-		this.add.image(0, 0, 'suelo').setOrigin(0, 0);
+		//this.add.image(0, 0, 'suelo').setOrigin(0, 0);
+
+		//Tilemap
+		const map = this.make.tilemap({ key: 'mapa1' });
+		const tileset = map.addTilesetImage('mapTiles', 'tileset');
+		const sueloLayer = map.createLayer('suelo', tileset);
+		const paredLayer = map.createLayer('pared', tileset);
+
+		paredLayer.setCollisionByProperty({ collides: true });
+
+		sueloLayer.setScale(2);
+		paredLayer.setScale(2);
 		
 		//Instanciamos nuestro personaje, que es un caballero, y la plataforma invisible que hace de suelo
-		let player = new Player(this, 100, 100);
+		let player = new Player(this, 200, 200);
+		player.setScale(0.5);
+
+
 		let floor = new Floor(this, 50);
 
 		let scene = this; // Nos guardamos una referencia a la escena para usarla en la función anidada que viene a continuación
-		
-	
+
 	}
 
 	init(){
