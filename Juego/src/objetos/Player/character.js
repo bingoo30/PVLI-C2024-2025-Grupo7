bffba1163afc2415f205677f88336b64fd7a36e9
Bingcheng Wang <biwang01@ucm.es>
2024-10-25 17:22:32 +0200

    character actualizado

--- Juego/src/objetos/Player/character.js
+++ Juego/src/objetos/Player/character.js
@@ -1,6 +1,9 @@
+import { Vector } from "matter";
+
 //constantes por defecto
 const DEFAULT_LIFE = 10;
 const DEFAULT_SPEED = 1;
+const DEFAULT_SPEEDFACTOR = 400;
 const DEFAULT_DAMAGE = 1;
 const DEFAULT_SHOOTSPEED = 1;
 const DEFAULT_CRIT = 0;
@@ -10,50 +13,26 @@ export default class character extends Phaser.GameObjects.Sprite {
      * @param {Phaser.Scene} scene La escena del juego
      * @param {number} x Posici�n X
      * @param {number} y Posici�n Y
-     * @param {number} [life=DEFAULT_LIFE] Vida del personaje
-     * @param {number} [damage=DEFAULT_DAMAGE] Da�o que inflige el personaje
-     * @param {number} [velocidad=DEFAULT_SPEED] Velocidad de movimiento
-     * @param {number} [fDisparo=DEFAULT_SHOOTSPEED] Velocidad de disparo
-     * @param {number} [prob=DEFAULT_CRIT] Probabilidad de ataque cr�tico
+     * @param {number} life Vida del personaje
+     * @param {number} damage Da�o que inflige el personaje
+     * @param {Vector} speed direccion del movimiento
+     * @param {number} speedFactor Velocidad de movimiento
+     * @param {number} shootspeed Velocidad de disparo
+     * @param {number} prob Probabilidad de ataque cr�tico
      */
-    constructor(scene, x, y,
-        life = DEFAULT_LIFE,
-        damage = DEFAULT_DAMAGE,
-        velocidad = DEFAULT_SPEED,
-        fDisparo = DEFAULT_SHOOTSPEED,
-        prob = DEFAULT_CRIT) {
+    constructor(scene, x, y, life, damage, speed, speedFactor, shootSpeed, prob) {
         super(scene, x, y, 'character');
+        // Lo agrego a la escena
         scene.add.existing(this);
+        // Agregamos fisicas
+        scene.physics.add.existing(this);
 
-        
         // Inicializar las propiedades
-        this.life = life;
-        this.damage = damage; 
-        this.velocidad = velocidad;
-        this.fDisparo = fDisparo;
-        this.prob = prob;
-    }
-    /**
-     * M�todo gen�rico para incrementar el valor de un atributo espec�fico
-     * 
-     * @param {string} attribute El nombre del atributo a modificar.
-     * @param {number} amount La cantidad a incrementar.
-     */
-    incrementAttribute(attribute, amount) {
-        if (this.hasOwnProperty(attribute) && typeof this[attribute] === 'number') {
-            this[attribute] += amount;
-        }
-    }
-    /**
-     * Funci�n para restar vida
-     * Para que no sea negativo cojo el maximo entre la resta y el 0
-     */
-    reduceLife(amount) {
-        this.life = Math.max(0, this.life - amount);
-    }
-
-    characterShoot(){
-        console.log("disparo, pium pium");
-
+        this.speed = speed || DEFAULT_SPEED;
+        this.speedFactor = speedFactor || DEFAULT_SPEEDFACTOR;
+        this.shootSpeed = shootSpeed ||DEFAULT_SHOOTSPEED;
+        this.life = life || DEFAULT_LIFE;
+        this.damage = damage || DEFAULT_DAMAGE;
+        this.prob = prob || DEFAULT_CRIT;
     }
 }
