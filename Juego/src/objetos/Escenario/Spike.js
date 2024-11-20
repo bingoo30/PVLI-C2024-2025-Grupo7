83fe4e9be0c4ac86b7bc4a87867191384b21f103
Bingcheng Wang <biwang01@ucm.es>
2024-11-20 18:03:48 +0100

    clase pincho estatica creada

---
+++ Juego/src/objetos/Escenario/Spike.js
@@ -0,0 +1,19 @@
+import Obstacle from "./Obstacle";
+
+const SPIKE_DAMAGE = 5;
+/**
+* @extends Obstacle
+*/
+//pinchos estaticos
+export default class Spike extends Obstacle {
+    constructor(scene, x, y) {
+        super(scene, x, y, "Spike");
+        this.scene.physics.add.existing(this);
+        //this.body.setSize(16, 8);
+        //this.body.setOffset(8, 24);
+        this.init(SPIKE_DAMAGE);
+    }
+    init(damage) {
+        super.init(damage);
+    }
+}
