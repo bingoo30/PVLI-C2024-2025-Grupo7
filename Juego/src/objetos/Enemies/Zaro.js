6c10af914a7177a8c75ea2de891ea31b569c5f7f
MauroUCM <maumar03@ucm.es>
2024-11-22 18:56:23 +0100

    Update Zaro.js

--- Juego/src/objetos/Enemies/Zaro.js
+++ Juego/src/objetos/Enemies/Zaro.js
@@ -1,6 +1,6 @@
 import Crac from './Crac.js';
 
-const TEELEPORT_RANGE = 700;
+const TELEPORT_RANGE = 700;
 const TELEPORT_COOLDOWN = 3000; 
 
 export default class Zaro extends Crac {
@@ -10,11 +10,20 @@ export default class Zaro extends Crac {
         this.player = player;
         this.scene.add.existing(this);
         this.init(0, 500, 5, 2, 0);
+        this.teleportCont = 3000;
     }
     init(speedFactor, shootSpeed, life, damage, prob) {
         super.init(speedFactor, shootSpeed, life, damage, prob);
     }
     preUpdate(t, dt) {
         super.preUpdate(t, dt);
+
+        if(this.getDistance() < TELEPORT_RANGE && this.teleportCont < 0){
+            console.log("jsdfnajfn");
+
+            this.teleportCont = TELEPORT_COOLDOWN;
+        }
+
+        this.teleportCont = this.teleportCont - dt;
     }
 }
