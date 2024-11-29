9ca0df98b1b8ad501023820171004a9bfb1532b4
Haoshuang <haohou@ucm.es>
2024-11-29 18:47:44 +0100

    Update zaro.js

--- Juego/src/objects/enemies/zaro.js
+++ Juego/src/objects/enemies/zaro.js
@@ -15,39 +15,46 @@ export default class Zaro extends Enemy {
         this.scene = scene;
         this.player = player;
         this.scene.add.existing(this);
-        this.init(0, 500, 5, 2, 0);
-        this.teleportCont = 3000;		
+        this.init(150, 300, 2, 2, 0);
+        //this.teleportCont = 3000;		
         this.shootCont = 0;
-        this.followRange = 0;
+        //this.followRange = 0;
+
+        let timer = this.scene.time.addEvent({
+            delay: 3000,
+            callback: this.changeVisible,
+            callbackScope: this,
+            loop: true
+        });
+
     }
     init(speedFactor, shootSpeed, life, damage, prob) {
         super.init(speedFactor, shootSpeed, life, damage, prob);
     }
     preUpdate(t, dt) {
         super.preUpdate(t, dt);
-        
-        if(this.shootCont <= 0 && this.getDistance() < SHOOTING_RANGE){
-            this.shootCont = SHOOTING_COOLDOWN;
-            fire(this, this.player, this.damage, this.shootSpeed, 'Bala', 4, this.pool);
-        }
-        this.shootCont = this.shootCont - dt;
 
-        if(this.getDistance() < TELEPORT_RANGE && this.teleportCont < 0){
-            this.teleportNearPlayer();
-            this.teleportCont = TELEPORT_COOLDOWN;
+        if (this.visible) {
+            if (this.shootCont <= 0 && this.getDistance() < SHOOTING_RANGE) {
+                this.shootCont = SHOOTING_COOLDOWN;
+                fire(this, this.player, this.damage, this.shootSpeed, 'Bala', 4, this.pool);
+            }
+            this.shootCont = this.shootCont - dt;
         }
 
-        this.teleportCont = this.teleportCont - dt;
-         
     }
-
+    changeVisible() {
+        if (this.visible) this.setVisible(false);
+        else this.setVisible(true);
+    }
+    
     getDistance(){
 		var p1 = this.x - this.player.x;
 		var p2 = this.y - this.player.y;
 
 		return Math.sqrt(p1*p1 + p2*p2);
-	}
-
+    }
+    /*
     teleportNearPlayer(dt)
     {
         this.x = (Math.random() * ((this.player.x - TELEPORT_MAX_DISTANCE) - this.player.x + TELEPORT_MAX_DISTANCE)) + (this.player.x - TELEPORT_MAX_DISTANCE);
@@ -55,4 +62,5 @@ export default class Zaro extends Enemy {
         // console.log("Teletransportandome a:" +  this.x + "/" + this.y);
         // console.log("El player está en:" + this.player.x + "/" + this.player.y);
     }
+    */
 }
