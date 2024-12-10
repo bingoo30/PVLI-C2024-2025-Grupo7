3ac8722412ee062462c01ad560ee6bfa38f9a7d0
Bingcheng Wang <biwang01@ucm.es>
2024-12-10 13:09:14 +0100

    mini fix

--- Juego/src/scenes/levels/level_1.js
+++ Juego/src/scenes/levels/level_1.js
@@ -325,7 +325,7 @@ export default class Animation extends Phaser.Scene {
 		// #region torretas
 		toAdds = [];
 		this.playerTurrent = new Pool(this, 10, 'Turret');
-		for (let i = 0; i < MAX; i++) {
+		for (let i = 0; i < 10; i++) {
 			let toAdd = new Turret(this, 0, 0, this.enemies);
 			toAdd.setPool(this.playerBullets);
 			toAdds.push(toAdd);
