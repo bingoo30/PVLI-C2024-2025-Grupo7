ff697b037a3c332b78bf806aaeaded1e007dc430
Haoshuang <haohou@ucm.es>
2024-12-13 03:29:33 +0100

    Update player.js

--- Juego/src/objects/player/player.js
+++ Juego/src/objects/player/player.js
@@ -188,6 +188,7 @@ export default class Player extends Character {
 
         // #region inventorios
         this.Inventory = player.Inventory;
+        this.Inventory.key = false;
         // #endregion
 
         // #region puntos de control status
